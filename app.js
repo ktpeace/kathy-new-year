cfd9234a8222960de4d0336a3e1cfee5131427e6
ktpeace <katpeace@protonmail.com>
2023-01-01 03:03:40 -0500

    temporary fix

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
-let now = new Date().toLocaleTimeString("en-US", {
-  timeZone: "America/Los_Angeles",
-});
-const timer = document.querySelector(".timer");
-timer.innerHTML = calculateTime();
+// let now = new Date().toLocaleTimeString("en-US", {
+//   timeZone: "America/Los_Angeles",
+// });
+// const timer = document.querySelector(".timer");
+// timer.innerHTML = calculateTime();
 
-function calculateTime() {
-  now = new Date().toLocaleTimeString("en-US", {
-    timeZone: "America/Los_Angeles",
-  });
-  const nums = now.split(/:| /); // ['9', '53', '42', 'PM']
-  let hours = 11 - nums[0];
-  let mins = 60 - nums[1];
-  let secs = 60 - nums[2];
-  if (nums[3] === "AM") {
-    const h1 = document.querySelector("h1");
-    h1.innerText = "🎆🌟HAPPY 2023, KATHY!🌟🎆";
-    const belowH1 = document.querySelector(".below-h1");
-    belowH1.innerHTML =
-      '<img src="sheep.png" alt="rabbit riding sheep while yelling onward" class="sheep">';
-  } else {
-    if (mins.toString().length === 1) mins = `0${mins}`;
-    if (secs.toString().length === 1) secs = `0${secs}`;
-    return `${hours}:${mins}:${secs}`;
-  }
-}
+// function calculateTime() {
+//   now = new Date().toLocaleTimeString("en-US", {
+//     timeZone: "America/Los_Angeles",
+//   });
+//   const nums = now.split(/:| /); // ['9', '53', '42', 'PM']
+//   let hours = 11 - nums[0];
+//   let mins = 60 - nums[1];
+//   let secs = 60 - nums[2];
+//   if (nums[3] === "AM") {
+const h1 = document.querySelector("h1");
+h1.innerText = "🎆🌟HAPPY 2023, KATHY!🌟🎆";
+const belowH1 = document.querySelector(".below-h1");
+belowH1.innerHTML =
+  '<img src="sheep.png" alt="rabbit riding sheep while yelling onward" class="sheep">';
+//   } else {
+//     if (mins.toString().length === 1) mins = `0${mins}`;
+//     if (secs.toString().length === 1) secs = `0${secs}`;
+//     return `${hours}:${mins}:${secs}`;
+//   }
+// }
 
-function time() {
-  timer.innerHTML = calculateTime();
-}
+// function time() {
+//   timer.innerHTML = calculateTime();
+// }
 
-setInterval(time, 1000);
+// setInterval(time, 1000);
